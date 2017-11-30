1eb2e675a0d725880c2150b5e2512f70ae8e3471
Sheng Jiang <jiang@thenetcircle.com>
2017-11-30 18:11:16 +0800

    Initial commit

---
+++ app/controller/upload.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+const fs = require('fs');
+const path = require('path');
+const awaitWriteStream = require('await-stream-ready').write;
+const sendToWormhole = require('stream-wormhole');
+
+class UploadController extends Controller {
+  async upload() {
+    const ctx = this.ctx;
+    const stream = await ctx.getFileStream();
+    const filename = encodeURIComponent(stream.fields.title) + path.extname(stream.filename).toLowerCase();
+
+    console.log('GOT' + filename);
+    const target = path.join(this.config.baseDir, 'app/public', filename);
+    const writeStream = fs.createWriteStream(target);
+    console.log('Save to' + writeStream);
+
+    try {
+      await awaitWriteStream(stream.pipe(writeStream));
+    } catch (err) {
+      await sendToWormhole(stream);
+      throw err;
+    }
+    this.ctx.redirect('/public/' + filename);
+  }
+}
+
+module.exports = UploadController;
---
+++ config/config.default.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1512027824297_1799';
+
+  // add your config here
+  config.middleware = [];
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.html': 'nunjucks',
+    },
+  };
+
+  config.security = {
+    csrf: {
+      ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
+    },
+  };
+
+  config.multipart = {
+    fileSize: '10mb',
+  };
+
+  return config;
+};
